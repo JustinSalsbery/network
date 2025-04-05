3772ca69c68c5aac809f4830727c36e7aee09db5
JustinSalsbery <justin.salsbery14@gmail.com>
2025-04-05 22:14:33 +0000

    misc: Added example-4

--- scripts/network/example-2.py
+++ scripts/network/example-2.py
@@ -6,7 +6,7 @@ from src.configurator import *
 iface_0 = Iface("169.254.0.0/24")
 iface_1 = Iface("169.254.1.0/24")
 
-client_0 = Client(do_forward=True)
+client_0 = Client(forward=True)
 client_0.add_iface(iface_0, "169.254.0.1")
 client_0.add_iface(iface_1, "169.254.1.1")
 
